2aa474341bae56c8c60042f479978bc1ab06fd67
Christian Riboldi <cmriboldi@gmail.com>
2018-06-21 09:32:49 -0600

    Creates message that will conflict.

--- TeachingGithub/ViewController.swift
+++ TeachingGithub/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
         
-        // Hotdog // hello
+        // Hotdog // hello This is a commit message that will cause a conflict
 
     }
 
